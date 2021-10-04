f7d8c257f86c959c9aa3a3aa9531ab2cc0b2c413
VL4D1K01k <vlad.nicorici.16@gmail.com>
2021-10-04 14:55:07 +0300

    Getting the Index when using ngFor

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -12,6 +12,7 @@ export class TaskComponent  {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    //this.log.push(this.log.length + 1); 
+    this.log.push(new Date());
   }
 }
